011e28983d4033fc5a3a21b757c2d1652398d300
Vladislav Soprun <develop@soprun.com>
2020-06-10 19:03:10 +0300

    commit...

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,19 +4,7 @@ import './Layout.css';
 function Layout() {
   return (
     <div className="container">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Service maintenance soprun.com.
     </div>
   );
 }
